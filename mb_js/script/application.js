93514de77381dbd69f48f5ab979ea9b860a1628f
jose <jose@kcom>
2019-12-26 19:26:10 -0300

    first commit.

---
+++ mb_js/script/application.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+let host="/rest"
+
+$(document).ready(function(){	
+		$("#search").on("click" , () => {
+			$("#result").html("");
+			let name = $('#name').val();
+			$.get (
+			`${host}/artist/artist_name/${name}`
+			,function (data){
+				data.map(row => {
+							let template_artist_result = `<tr><td class="id_result">${row.artistId}</td><td class="name_result">${row.name}</td><td class="name_result">${row.sortName}</td><td class="gid_result">${row.gid}</td></tr>`;
+							return template_artist_result;
+				}).forEach( 
+						element => { 
+									$("#result").append(element);
+									return;
+									}
+						);
+			});
+		}
+	);
+});
