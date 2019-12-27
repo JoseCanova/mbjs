e0e0f1df5c9cfb67a5a0b3ad5dc9348083a64c61
jose <jose@kcom>
2019-12-27 02:30:32 -0300

    commit.

--- mb_js/script/application.js
+++ mb_js/script/application.js
@@ -2,20 +2,32 @@
  * 
  */
 
-let host="/rest"
-
-$(document).ready(function(){	
+let host="/rest";
+var current_result;
+mount_result = function(data){ 
+	current_result = data;
+	$("#result").append( 
+			data.map(row => {
+				let template_artist_result = `<tr>
+													<td class="td-artist-id">${row.artistId}</td>
+													<td class="name_result">${row.name}</td>
+													<td class="name_result">${row.sortName}</td>
+													<td class="td-artist-gid">${row.gid}</td>
+												</tr>`;
+				return template_artist_result;
+	}));
+	$(".div-result").css("display" , "block");
+}
+$(document).ready(function(){
 		$("#search").on("click" , () => {
+			$(".div-result").css("display" , "none");
 			$("#result").html("");
 			let name = $('#name').val();
 			$.get (
 			`${host}/artist/artist_name/${name}`
 			,function (data){
-				$("#result").append( 
-						data.map(row => {
-							let template_artist_result = `<tr><td class="id_result">${row.artistId}</td><td class="name_result">${row.name}</td><td class="name_result">${row.sortName}</td><td class="gid_result">${row.gid}</td></tr>`;
-							return template_artist_result;
-				}));
+				current_result = data;
+				mount_result(data);
 			});
 		}
 	);
