fe3f63b234a8693c7491914985ef82fa5d80336a
jose <jose@kcom>
2019-12-28 20:50:22 -0300

    using class to prepare the view.

--- mb_js/js/application.js
+++ mb_js/js/application.js
@@ -1,23 +1,43 @@
 /**
  * 
  */
+
+class Artist{
+	constructor (id,name,sortName,gid){ 
+		this.id = id; 
+		this.name = name; 
+		this.sortName = sortName; 
+		this.gid = gid;
+	}
+};
+
+class ArtistView extends Artist { 
+	
+	constructor (id,name,sortName,gid) { 
+		super(id,name,sortName,gid);
+	}
+	
+	render (){ 
+		return `<div class="row">
+					<div class="col">${this.id}</div>
+					<div class="col-4">${this.name}</div>
+					<div class="col-4">${this.sortName}</div>
+					<div class="col">
+						<div class="gid-result">
+							<span  title="${this.gid}">${this.gid}</span>
+						</div>
+					</div>
+				</div>`;
+	}
+}
+
 let host="/rest";
 var current_result;
 mount_result = function(data){ 
 	current_result = data;
 	$("#result").append( 
 			data.map(row => {
-				let template_artist_result = `<div class="row">
-													<div class="col">${row.artistId}</div>
-													<div class="col-4">${row.name}</div>
-													<div class="col-4">${row.sortName}</div>
-													<div class="col">
-														<div class="gid-result">
-															<span  title="${row.gid}">${row.gid}</span>
-														</div>
-													</div>
-												</div>`;
-				return template_artist_result;
+				return new ArtistView(row.artistId , row.name , row.sortName, row.gid).render();
 			}));
 };
 
