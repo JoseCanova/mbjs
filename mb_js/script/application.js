c0e4ed56908d14803ccba3067330ad905cab9294
jose <jose@kcom>
2019-12-28 18:17:05 -0300

    remove incorrect import

--- mb_js/script/application.js
+++ mb_js/script/application.js
@@ -8,24 +8,22 @@ mount_result = function(data){
 	current_result = data;
 	$("#result").append( 
 			data.map(row => {
-				let template_artist_result = `<tr>
-													<td class="td-artist-id">${row.artistId}</td>
-													<td class="name_result">${row.name}</td>
-													<td class="name_result">${row.sortName}</td>
-													<td class="td-artist-gid">
+				let template_artist_result = `<div class="row">
+													<div class="col">${row.artistId}</div>
+													<div class="col-4">${row.name}</div>
+													<div class="col-4">${row.sortName}</div>
+													<div class="col">
 														<div class="gid-result">
 															<span  title="${row.gid}">${row.gid}</span>
 														</div>
-													</td>
-												</tr>`;
+													</div>
+												</div>`;
 				return template_artist_result;
 	}));
-	$(".div-result").css("display" , "block");
 }
 
 $(document).ready(function(){
 		$("#search").on("click" , () => {
-			$(".div-result").css("display" , "none");
 			$("#result").html("");
 			let name = $('#name').val();
 			$.get (
