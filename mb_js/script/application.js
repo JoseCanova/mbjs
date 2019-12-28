b5bf944e212b51ade59662949815cd8a777250b8
jose <jose@kcom>
2019-12-28 19:18:51 -0300

    added nav-bar removed mdbjs

--- mb_js/script/application.js
+++ mb_js/script/application.js
@@ -1,7 +1,6 @@
 /**
  * 
  */
-
 let host="/rest";
 var current_result;
 mount_result = function(data){ 
@@ -19,19 +18,20 @@ mount_result = function(data){
 													</div>
 												</div>`;
 				return template_artist_result;
-	}));
-}
+			}));
+};
 
 $(document).ready(function(){
-		$("#search").on("click" , () => {
+	$("#search1").on("click" , () => {
 			$("#result").html("");
-			let name = $('#name').val();
-			$.get (
-			`${host}/artist/artist_name/${name}`
-			,function (data){
-				current_result = data;
-				mount_result(data);
-			});
-		}
-	);
+				let name = $('#search-text').val();
+				$.get (
+						`${host}/artist/artist_name/${name}`
+						,function (data){
+							current_result = data;
+							mount_result(data);
+							$("#artist-container").removeClass("d-none");
+						});
+				}
+			);
 });
